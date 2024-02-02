1d34ebdf2f91d0d2eeb539444d4d4e4a39ffbe7b
akasumv <akasum99@gmail.com>
2024-02-03 00:36:53 +0400

    оптимизировал

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,12 +4,11 @@ import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl userDaoJDBCImpl;
+    private final UserDaoJDBCImpl userDaoJDBCImpl;
     public UserServiceImpl() {
         try {
             this.userDaoJDBCImpl = new UserDaoJDBCImpl(new Util().getConnection());
@@ -25,7 +24,7 @@ public class UserServiceImpl implements UserService {
     }
     public void saveUser(String name, String lastName, byte age) {
         userDaoJDBCImpl.saveUser(name, lastName, age);
-        System.out.println(String.format("User с именем – %s добавлен в базу данных", name));
+        System.out.printf("User с именем – %s добавлен в базу данных%n", name);
     }
     public void removeUserById(long id) {
         userDaoJDBCImpl.removeUserById(id);
